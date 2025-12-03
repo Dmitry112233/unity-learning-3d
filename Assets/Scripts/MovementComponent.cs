37e9b04304e88de3c70d9f161256fe44bda86a95
Dmitry Starovoytov <>
2025-12-04 00:30:41 +0300

    implemented nav mesh movement.

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -1,18 +1,86 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MovementComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public WayPoint current;
+    private LinkedListNode<WayPoint> currentNode;
+    private bool isMovingForward = true;
+    private WayPointManager route;
+    
+    private const float reachDistance = 1.5f;
+    
+    private NavMeshAgent agent;
+    
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
         
+        if (current != null)
+            SetCurrent(current);
+    }
+    
+    public void SetCurrent(WayPoint wp)
+    {
+        current = wp;
+        route = wp.ownerRoute;
+        currentNode = route.NodeOf(wp);
+
+        agent.SetDestination(wp.transform.position);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (current == null || agent.pathPending)
+            return;
+
+        float dist = Vector3.Distance(transform.position, current.transform.position);
+        Debug.Log(dist);
+
+        if (dist < reachDistance)
+        {
+            ChooseNext();
+        }
+    }
+
+    private void ChooseNext()
+    {
+        if (current.branches.Count > 0)
+        {
+            if (Random.value < 0.9f) 
+            {
+                WayPoint branchTarget = current.branches[Random.Range(0, current.branches.Count)];
+                SetCurrent(branchTarget);
+                return;
+            }
+        }
+
+        if (isMovingForward)
+        {
+            if (currentNode.Next != null)
+            {
+                SetCurrent(currentNode.Next.Value);
+            }
+            else
+            {
+                isMovingForward = false;
+                if (currentNode.Previous != null)
+                    SetCurrent(currentNode.Previous.Value);
+            }
+        }
+        else
+        {
+            if (currentNode.Previous != null)
+            {
+                SetCurrent(currentNode.Previous.Value);
+            }
+            else
+            {
+                isMovingForward = true;
+                if (currentNode.Next != null)
+                    SetCurrent(currentNode.Next.Value);
+            }
+        }
     }
 }
